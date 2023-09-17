e1eb8d187aac5c566671eb8823f399921074e449
temple69 <utemple169@gmail.com>
2023-09-17 17:59:43 +0100

    Home Component

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Advice from "./Components/Advice"; // <--- import Advice component
+//initailize App component
+function App() {
+  return (
+    <>
+      <Advice />
+    </>
+  );
+}
+// <--- export component
+export default App;
